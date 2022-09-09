0b817259ee8cafdef051289aa4ae75246fddf73b
Riley Flynn <riley@rileyflynn.me>
2022-09-09 20:07:32 -0230

    Problem 0 - Smoke test

---
+++ 0-smoke-test/smoketest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func handleConn(conn net.Conn) {
+	log.Printf("New connection: %s", conn.RemoteAddr())
+
+	_, err := io.Copy(conn, conn)
+	if err != nil {
+		log.Printf("Error copying data: %s", err)
+	}
+
+	conn.Close()
+}
+
+func main() {
+	listener, _ := net.Listen("tcp", ":3000")
+
+	for {
+		conn, _ := listener.Accept()
+		go handleConn(conn)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nint8835/protohackers
+
+go 1.19
